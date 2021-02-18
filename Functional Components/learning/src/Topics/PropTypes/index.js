600b12c4eb5ef122b24e27c8ed0a7bbf07edd4c1
skosana721 <wonganiskosana@gmail.com>
2021-02-18 11:56:42 +0200

    imported useFetch custom hooks and used it to access data that is coming from an api

--- Functional Components/learning/src/Topics/PropTypes/index.js
+++ Functional Components/learning/src/Topics/PropTypes/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import User from "./User";
+import { useFetch } from "../custom-hooks/useFetch";
 
+const URL = "https://jsonplaceholder.typicode.com/users";
 function Index() {
-  return <div></div>;
+  const { user } = useFetch(URL);
+  return (
+    <div>
+      <section className="users">
+        {user.map((item) => {
+          return <User key={item.id} {...item} />;
+        })}
+      </section>
+    </div>
+  );
 }
 
 export default Index;
