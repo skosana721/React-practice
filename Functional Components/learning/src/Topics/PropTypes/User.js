55bfa6dad45db6a3fbe935f360ebc84708a05159
skosana721 <wonganiskosana@gmail.com>
2021-02-18 11:59:39 +0200

    passed name as prop to User and displayed it and also imported PropTypes to check if all the props that are being passed are present on all object

---
+++ Functional Components/learning/src/Topics/PropTypes/User.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+const User = ({ name }) => {
+  return (
+    <article>
+      <li>
+        <h4>{name}</h4>
+      </li>
+    </article>
+  );
+};
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+export default User;
