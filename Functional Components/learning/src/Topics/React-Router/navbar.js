a210e4923b433d8e6651708aada7b75e8f17a94c
skosana721 <wonganiskosana@gmail.com>
2021-02-18 15:22:14 +0200

    imported Link from react-router-dom link it to the home component and about component

--- Functional Components/learning/src/Topics/React-Router/navbar.js
+++ Functional Components/learning/src/Topics/React-Router/navbar.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <div className="nav-links">
+      <Link to="/">
+        <h3>
+          <a href="#">Home</a>
+        </h3>
+      </Link>
+      <Link to="/about">
+        <h3>
+          <a>About</a>
+        </h3>
+      </Link>
+
+      <Link to="/">
+        <h3>
+          <a href="">Home</a>
+        </h3>
+      </Link>
+    </div>
+  );
 }
 
 export default Navbar;
