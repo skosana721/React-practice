69dbeeac9aead8055fd01f0dde2e981cd941fca4
skosana721 <wonganiskosana@gmail.com>
2021-02-18 14:30:40 +0200

    render all component to index.js

--- Functional Components/learning/src/Topics/React-Router/index.js
+++ Functional Components/learning/src/Topics/React-Router/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import About from "./about";
+import Error from "./error";
+import Home from "./home";
+import Navbar from "./navbar";
 
 function Index() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>React Router</h2>
+      <Navbar />
+      <Home />
+      <About />
+      <Error />
+    </div>
+  );
 }
 
 export default Index;
