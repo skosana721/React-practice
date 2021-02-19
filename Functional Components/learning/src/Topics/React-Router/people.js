fff09da3be065b1f3cdce9385c9fd29d56ebf8f2
skosana721 <wonganiskosana@gmail.com>
2021-02-19 09:28:56 +0200

    removed the console log

--- Functional Components/learning/src/Topics/React-Router/people.js
+++ Functional Components/learning/src/Topics/React-Router/people.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import { data } from "../../data";
+import { Link } from "react-router-dom";
 
 function People() {
   const [people, setPeople] = useState(data);
-  console.log(people);
+
   return (
     <div>
       {people.map((person) => {
         return (
           <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <Link to={`/person/${person.id}`}>Learn More</Link>
           </div>
         );
       })}
