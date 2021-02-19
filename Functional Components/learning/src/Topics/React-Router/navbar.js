fc9589ed37368242442e1e73ed8fec0bb0139001
skosana721 <wonganiskosana@gmail.com>
2021-02-19 08:17:36 +0200

    remove an a element

--- Functional Components/learning/src/Topics/React-Router/navbar.js
+++ Functional Components/learning/src/Topics/React-Router/navbar.js
@@ -4,22 +4,9 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="nav-links">
-      <Link to="/">
-        <h3>
-          <a href="#">Home</a>
-        </h3>
-      </Link>
-      <Link to="/about">
-        <h3>
-          <a>About</a>
-        </h3>
-      </Link>
-
-      <Link to="/">
-        <h3>
-          <a href="">Home</a>
-        </h3>
-      </Link>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/people">People</Link>
     </div>
   );
 }
