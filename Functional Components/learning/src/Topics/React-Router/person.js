ed232f3cc9f2b9a060cd6126e8e7e776ea9dcf03
skosana721 <wonganiskosana@gmail.com>
2021-02-19 09:31:04 +0200

    used useEffect to filter out if id the date.js matches the id in teh useParams

--- Functional Components/learning/src/Topics/React-Router/person.js
+++ Functional Components/learning/src/Topics/React-Router/person.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { data } from "../../data";
+import { Link, useParams } from "react-router-dom";
 
-function person() {
+function Person() {
+  const { id } = useParams();
+  const [name, setName] = useState("default Name");
+  useEffect(() => {
+    const NewPerson = data.filter((person) => person.id === parseFloat(id));
+
+    setName(NewPerson[0].name);
+  }, [id]);
+
   return (
     <div>
-      <h2>Person</h2>
+      <h2>{name}</h2>
+
+      <Link to="/people" className="btn">
+        Back To People
+      </Link>
     </div>
   );
 }
 
-export default person;
+export default Person;
